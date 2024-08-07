69972b9b0eeeecffc640941e7ff2c2e96ad6bc00
Marius Storhaug <marstor@hotmail.com>
2024-08-07 03:25:34 +0200

    Fix

--- tests/IssueBody.Tests.ps1
+++ tests/IssueBody.Tests.ps1
@@ -4,12 +4,12 @@
 
 Describe "IssueBody" {
     It "'Type with spaces' should contain 'Action'" {
-        $data | Should -ContainKey 'Type with spaces'
+        $data.Keys | Should -Contain 'Type with spaces'
         $data['Type with spaces'] | Should -Be 'Action'
     }
 
     It "'Multiline' should contain a multiline string with 3 lines" {
-        $data | Should -ContainKey 'Multiline'
+        $data.Keys | Should -Contain 'Multiline'
         $data['Multiline'] | Should -Be @'
 test
 is multi
@@ -18,7 +18,7 @@ line
     }
 
     It "'OS' should contain a hashtable with 3 items" {
-        $data | Should -ContainKey 'OS'
+        $data.Keys | Should -Contain 'OS'
         $data['OS'] | Should -Be @{
             Windows = $true
             Linux   = $true
