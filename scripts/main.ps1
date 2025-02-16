e99a9e849ea3cdf6ca162f9990864f026787c414
Marius Storhaug <marstor@hotmail.com>
2025-02-16 14:59:07 +0100

    Fixes (#8)
    ## Description
    
    This pull request includes several updates to configuration files and
    workflows, primarily focused on improving code linting, formatting, and
    workflow execution. The most important changes include updates to
    `.gitattributes`, the addition of linter configuration files,
    modifications to GitHub Actions workflows, and updates to the
    `action.yml` and `scripts/main.ps1` files.
    
    ### Configuration and Linting Improvements:
    
    *
    [`.gitattributes`](diffhunk://#diff-618cd5b83d62060ba3d027e314a21ceaf75d36067ff820db126642944145393eL1-R15):
    Updated to set `eol` for various file types and ensure binary files are
    treated correctly.
    *
    [`.github/linters/.jscpd.json`](diffhunk://#diff-557094e283c00b23265c1c75872f41c6b1a524a00f0d99dd68ebd22cb63bfdd6R1-R10):
    Added configuration for JavaScript Copy/Paste Detector (jscpd).
    *
    [`.github/linters/.markdown-lint.yml`](diffhunk://#diff-2fd3619d2b7372123257bc21645edb90c24293e66babbc6053033a89e21bab34R1-R25):
    Added configuration for markdown linter rules.
    *
    [`.github/linters/.powershell-psscriptanalyzer.psd1`](diffhunk://#diff-aae69c9d6774628ed181eacf53aee0f38eb6c2f53492cf3a5b7f7bdb6ef43b6aR1-R56):
    Added configuration for PowerShell Script Analyzer rules.
    
    ### Workflow Updates:
    
    *
    [`.github/workflows/Action-Test.yml`](diffhunk://#diff-a12ae5c885b0673c0ff6f70c2670886907590d624626e07da4c52e01aeaf56a4L33-R34):
    Modified to use a script directly and updated the action to use
    `Invoke-Pester` for testing.
    [[1]](diffhunk://#diff-a12ae5c885b0673c0ff6f70c2670886907590d624626e07da4c52e01aeaf56a4L33-R34)
    [[2]](diffhunk://#diff-a12ae5c885b0673c0ff6f70c2670886907590d624626e07da4c52e01aeaf56a4L42-L46)
      - Fixes #7
    *
    [`.github/workflows/Linter.yml`](diffhunk://#diff-482e65806ed9e4a7320f14964764086b91fed4a28d12e4efde1776472e147e79L27-R30):
    Updated to use the latest version of `super-linter` and disable certain
    prettier validations.
    
    ### Documentation and Licensing:
    
    *
    [`LICENSE`](diffhunk://#diff-c693279643b8cd5d248172d9c22cb7cf4ed163a3c98c8a3f69c2717edd3eacb7L3-R3):
    Updated the copyright year to 2025.
    *
    [`README.md`](diffhunk://#diff-b335630551682c19a781afebcf4d07bf978fb1f8ac04c6bf87428ed5106870f5L56-R56):
    Clarified the description of the `IssueBody` input in the documentation.
    
    ### Action and Script Updates:
    
    *
    [`action.yml`](diffhunk://#diff-1243c5424efaaa19bd8e813c5e6f6da46316e63761421b3e5f5c8ced9a36e6b6L10-R10):
    Updated the description for `IssueBody` and modified the script
    execution method.
    [[1]](diffhunk://#diff-1243c5424efaaa19bd8e813c5e6f6da46316e63761421b3e5f5c8ced9a36e6b6L10-R10)
    [[2]](diffhunk://#diff-1243c5424efaaa19bd8e813c5e6f6da46316e63761421b3e5f5c8ced9a36e6b6L29-R31)
    *
    [`scripts/main.ps1`](diffhunk://#diff-dc2e5a659836b1b73abb03421c567f5018c2755677c4a0aa764cb26117b68011L6-R13):
    Simplified the parameter usage by directly accessing environment
    variables.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -3,17 +3,14 @@
     Justification = 'Variable is used in LogGroup blocks.'
 )]
 [CmdletBinding()]
-param(
-    [Parameter()]
-    [string] $IssueBody = $env:GITHUB_ACTION_INPUT_IssueBody
-)
+param()
 
 LogGroup 'Issue Body - Raw' {
-    Write-Output $IssueBody
+    Write-Output $env:GITHUB_ACTION_INPUT_IssueBody
 }
 
 LogGroup 'Issue Body - Object' {
-    $data = $IssueBody | ConvertFrom-IssueForm
+    $data = $env:GITHUB_ACTION_INPUT_IssueBody | ConvertFrom-IssueForm
     $data | Format-List
     Set-GitHubOutput -Name 'data' -Value $data
 }
