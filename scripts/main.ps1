58bfa740a8d94f28a31081d324262c863732244c
Marius Storhaug <marstor@hotmail.com>
2025-03-02 09:46:05 +0100

    🩹 [Patch]: Update main.ps1 to format output as string for better readability

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -5,12 +5,14 @@
 [CmdletBinding()]
 param()
 
+$PSStyle.OutputRendering = 'Ansi'
+
 LogGroup 'Issue Body - Raw' {
     Write-Output $env:GITHUB_ACTION_INPUT_IssueBody
 }
 
 LogGroup 'Issue Body - Object' {
     $data = $env:GITHUB_ACTION_INPUT_IssueBody | ConvertFrom-IssueForm
-    $data | Format-List
+    $data | Format-List | Out-String
     Set-GitHubOutput -Name 'data' -Value $data
 }
